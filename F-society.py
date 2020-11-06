643567e6302227b71eb30dc51e1f21ba19cc5ebf
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2020-11-06 18:04:02 +0530

    Add files via upload

---
+++ F-society.py
@@ -0,0 +1,19 @@
+
+
+import os
+import discord
+from discord.ext import commands
+import asyncio
+
+client = commands.Bot(command_prefix = ':')
+
+
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ROOKS OFFICIAL"))
+#Runs when bot is online and active  
+  print("BOT IS READY")
+
+client.run(os.environ['DISCORD_TOKEN'])
+
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python F-society.py
