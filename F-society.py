08e7a12067cee3c0bb01b703ccd0f8ef17141bd2
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-25 17:47:47 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix = ':')
 
 @client.event
 async def on_ready():
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="s"))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="The Ded Chat ;-;"))
 #Runs when bot is online and active  
   print("BOT IS READY")
 
