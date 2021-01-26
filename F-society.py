4b20d496d5ec50e5909a68e1ac757d4f5506cb85
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 16:22:13 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -29,6 +29,23 @@ async def help(ctx, arg=None):
   embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/774143806601822208/778997559313301504/ezgif.com-gif-maker_2.gif')
   embed.add_field(name="Ping",value=":placard: So far I only have the Ping Command ;-; Blame it on <@261742964441612298>",inline=False)
   await ctx.send(embed=embed)
+
+em=discord.Embed(title = "The F Society", description= "**You have Been Bammed In F Society!**", colour=0x2f3136)
+em.set_footer(text="Dig Bick Energy Gang")
+em.set_thumbnail(url='https://cdn.discordapp.com/attachments/774143806601822208/778997559313301504/ezgif.com-gif-maker_2.gif')
+em.add_field(name="Reason",value=":placard: You are too cool to get a BAM <:okDamn:792390256980000788> Blame <@261742964441612298> For trolling you! <a:THINK_EXTREME:801464607159091201> ",inline=True)
+@client.command()
+async def bam(ctx,user_id=None,args=em):
+  
+  
+  if user_id!=None and args !=None:
+    try:
+      target = await client.fetch_user(user_id)
+      await target.send(embed=args)
+      
+      await ctx.send("The User is Bammed <:okDamn:792390256980000788> ")
+    except:
+      await ctx.channel.send("`Couldn't DM the given user`")
 client.run(os.environ['DISCORD_TOKEN'])
 
 
