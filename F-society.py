c6deef1b5f837b22e108ad171cf1a6a5d8a8d93a
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 12:27:20 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -13,7 +13,12 @@ async def on_ready():
   await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="QUACK taking over"))
 #Runs when bot is online and active  
   print("BOT IS READY")
-
+@client.command()
+async def ping(ctx, arg=None):
+  if arg=="Pong":
+    await ctx.send("Sexy job you just ponged yourself <a:shiba_Sexy_wink:794199806833590302> ")
+  else:
+    await ctx.send(f'<:wot:790094440387182604> Pong!{round(client.latency *1000)}ms')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
