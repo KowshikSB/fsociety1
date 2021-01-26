af875dda720b1b4cf34479d890ea13fdab12ff9b
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 12:19:38 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-client = commands.Bot(command_prefix = ':')
+client = commands.Bot(command_prefix = '^')
 
 
 @client.event
@@ -13,6 +13,8 @@ async def on_ready():
   await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="QUACK taking over"))
 #Runs when bot is online and active  
   print("BOT IS READY")
+
+
 
 client.run(os.environ['DISCORD_TOKEN'])
 
