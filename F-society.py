e4afdd9f49efd0ddd94e2342bfd08d9a37028d5c
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 13:40:39 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 import asyncio
 
 client = commands.Bot(command_prefix = '^')
+client.remove_command("help")
+
 
 
 @client.event
@@ -19,8 +21,13 @@ async def ping(ctx, arg=None):
     await ctx.send("Sexy job you just ponged yourself <a:shiba_Sexy_wink:794199806833590302> ")
   else:
     await ctx.send(f'<:wot:790094440387182604> Pong! `{round(client.latency *1000)}ms`')
-  
-
+@client.group(invoke_whithout_command=True)
+  async def help(ctx):
+    em = discord.Embed(title = "The F Society" , descritption = "**Help Commands**",color=#2f3136)
+    em.add_field(name="Ping",value="^ping - *So far we got Ping command ;-;* <@261742964441612298> *is Making me more useful* <:angry_flushed:803240935433437194>")
+    
+    await ctx.(Embed=em)
+ 
 
 client.run(os.environ['DISCORD_TOKEN'])
 
