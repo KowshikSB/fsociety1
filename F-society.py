fce89558c9e5eab13e83b545365821eb82b97b9b
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 14:32:06 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -28,7 +28,7 @@ async def help():
   embed.set_footer(text="Dig Bick Energy Gang")
   embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/774143806601822208/778997559313301504/ezgif.com-gif-maker_2.gif')
   embed.add_field(name="Ping",value=":placard: So far I only have the Ping Command ;-; Blame it on <@261742964441612298>",inline=False)
-  await client.say(embed=embed)
+  await help.say(embed=embed)
 client.run(os.environ['DISCORD_TOKEN'])
 
 
