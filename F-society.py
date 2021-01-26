0dd7b503060aa803ba0040ef7b6f1fee36a4fd94
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 14:28:29 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -23,11 +23,8 @@ async def ping(ctx, arg=None):
     await ctx.send(f'<:wot:790094440387182604> Pong! `{round(client.latency *1000)}ms`')
 @client.command()
 async def help():
-  embed=discord.Embed(
-    title = "The F Society"
-    description = "Help"
-    colour = "#2f3136"
-  )
+  embed=discord.Embed(title = "The F Society", description= "Help", colour=0x2f3136)
+  
   embed.set_footer(text="Dig Bick Energy Gang")
   embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/774143806601822208/778997559313301504/ezgif.com-gif-maker_2.gif')
   embed.add_field(name="Ping",value=":placard: So far I only have the Ping Command ;-; Blame it on <@261742964441612298>",inline=False)
