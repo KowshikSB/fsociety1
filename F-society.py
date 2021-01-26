2ac3da4ba12f1cdfde169a8419f684c3714992c7
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 13:18:24 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -4,35 +4,26 @@ import os
 import discord
 from discord.ext import commands
 import asyncio
-import random
 
 client = commands.Bot(command_prefix = '^')
 
 
-
+@client.event
+async def on_ready():
+  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="The Ded Chat ;-;"))
 #Runs when bot is online and active  
-  
+  print("BOT IS READY")
 @client.command()
 async def ping(ctx, arg=None):
   if arg=="Pong":
     await ctx.send("Sexy job you just ponged yourself <a:shiba_Sexy_wink:794199806833590302> ")
   else:
     await ctx.send(f'<:wot:790094440387182604> Pong! `{round(client.latency *1000)}ms`')
+  
 @client.event
-async def on_ready():
-  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="The Ded Chat ;-;"))
-  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="To KaZE"))
-  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="QUACK taking over"))
-  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="F SOCIETY"))
-  print("Ready")
-async def ch_pr():
-  await client.wait_until_ready()
-  statuses = [ "The Ded Chat ;-;"]
-  while not client.is_closed():
-    status=random.choice(statuses)
-    await client.change_presence(activity=discord.watching(name=status))
-    await asyncio.sleep(10)
-client.loop.create_task(ch_pr)
+async def on_member_join(member):
+  await member.send(f'**Welcome to the F Society!** @{member} *Read the* <#774143716042604545> *and get your roles in* <#775269400549916702> <:ShibaHeart:793851831431200808> <@&802760617769041990>')
+
 
 client.run(os.environ['DISCORD_TOKEN'])
 
