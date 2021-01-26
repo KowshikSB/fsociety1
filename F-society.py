b01ba8ae46e0e7bc450a3cb4661a99916455b8ad
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 12:57:07 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -18,7 +18,7 @@ async def ping(ctx, arg=None):
   if arg=="Pong":
     await ctx.send("Sexy job you just ponged yourself <a:shiba_Sexy_wink:794199806833590302> ")
   else:
-    await ctx.send(f'<:wot:790094440387182604> Pong! {round(client.latency *1000)}ms')
+    await ctx.send(f'<:wot:790094440387182604> Pong! `{round(client.latency *1000)}ms`')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
