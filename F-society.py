6b7746e710257dc449e9694d5f7ef4e782974b2c
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-26 13:42:06 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -22,11 +22,11 @@ async def ping(ctx, arg=None):
   else:
     await ctx.send(f'<:wot:790094440387182604> Pong! `{round(client.latency *1000)}ms`')
 @client.group(invoke_whithout_command=True)
-  async def help(ctx):
-    em = discord.Embed(title = "The F Society" , descritption = "**Help Commands**",color=#2f3136)
-    em.add_field(name="Ping",value="^ping - *So far we got Ping command ;-;* <@261742964441612298> *is Making me more useful* <:angry_flushed:803240935433437194>")
+async def help(ctx):
+  em = discord.Embed(title = "The F Society" , descritption = "**Help Commands**",color=#2f3136)
+  em.add_field(name="Ping",value="^ping - *So far we got Ping command ;-;* <@261742964441612298> *is Making me more useful* <:angry_flushed:803240935433437194>")
     
-    await ctx.(Embed=em)
+  await ctx.(Embed=em)
  
 
 client.run(os.environ['DISCORD_TOKEN'])
