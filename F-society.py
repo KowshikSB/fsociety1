2ea6ccde6760ced5216c8df5890d6da4d6d3f980
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-27 17:03:47 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -87,7 +87,7 @@ async def on_message_edit(before,after):
       client.esniped_messages[before.channel.id]=None
 
 @client.command()
-async def editsnipe(ctx,Aliases=['esnipe']):
+async def editsnipe(ctx):
     try:
       contents,author,channel_name,time=client.esniped_messages[ctx.channel.id]
       embed=discord.Embed(description=contents,color=0x2f3136,timestamp=time)
