1fa6438019abbf27f38fe99d9d90fb9c341d3a02
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-28 16:01:22 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -100,15 +100,14 @@ async def editsnipe(ctx):
     except:
       await ctx.channel.send("<a:potato_rage:788063034701906001> *Dont waste my ammo `;-;` Nothing to snipe!*")
 @client.command()
-async def avatar(ctx,avamember : discord.Member=None):
+async def avatar(ctx ,avamember : discord.Member=None):
   if avamember is None:
     avamember=ctx.message.author 
-    avamember=avamember.height(50)
-    avamember= avamember.width(50)
+    
   userAvatarUrl = avamember.avatar_url
   em = discord.Embed(title=f"Avatar of {avamember}", color=0x2f3136)
   em.set_footer(text=f'Requested by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-  em.set_image(url=userAvatarUrl) 
+  em.set_thumbnail(url=userAvatarUrl) 
     
   await ctx.send(embed=em) 
 @client.command()
