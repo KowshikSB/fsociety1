a61aad2c0ccf34ef9321f2853647304f0cddd593
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-28 11:51:35 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import asyncio
 import random
 
-client = commands.Bot(command_prefix = '^')
+client = commands.Bot(command_prefix = 'f ')
 client.remove_command("help")
 
 
