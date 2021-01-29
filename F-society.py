2dfa1f5c6f35b932777d31a53325aa5d2d600a0c
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-01-29 22:35:57 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -49,22 +49,25 @@ async def eiball(ctx, arg=None):
   await msg.add_reaction("<:wot:790094440387182604>") #\U0001f3b1
   
 @client.command()
-@commands.is_owner()
+
 async def bam(ctx,user_id=None,args=None):
-  em=discord.Embed(title = ctx.guild.name, description= "**You have Been Bammed In {}!**".format(ctx.guild.name), colour=0x2f3136)
-  em.set_footer(text="Dig Bick Energy Gang")
-  icon=ctx.guild.icon_url
-  em.set_thumbnail(url=icon)
-  em.add_field(name="Reason -",value=":placard: You are too cool to get a BAM <:okDamn:792390256980000788> Blame <@261742964441612298> For trolling you! <a:THINK_EXTREME:801464607159091201> ",inline=True)
-  args=em
-  if user_id!=None and args !=None:
-    try:
-      target = await client.fetch_user(user_id)
-      await target.send(embed=args)
+  if author.id==Guild.owner_id:
+    em=discord.Embed(title = ctx.guild.name, description= "**You have Been Bammed In {}!**".format(ctx.guild.name), colour=0x2f3136)
+    em.set_footer(text="Dig Bick Energy Gang")
+    icon=ctx.guild.icon_url
+    em.set_thumbnail(url=icon)
+    em.add_field(name="Reason -",value=":placard: You are too cool to get a BAM <:okDamn:792390256980000788> Blame <@261742964441612298> For trolling you! <a:THINK_EXTREME:801464607159091201> ",inline=True)
+    args=em
+    if user_id!=None and args !=None:
+      try:
+        target = await client.fetch_user(user_id)
+        await target.send(embed=args)
       
-      await ctx.send("The User is Bammed <:okDamn:792390256980000788> ")
-    except:
-      await ctx.channel.send("`Couldn't DM the given user`") 
+        await ctx.send("The User is Bammed <:okDamn:792390256980000788> ")
+      except:
+        await ctx.channel.send("`Couldn't DM the given user`") 
+  else:
+    print("You can't Bam noob!")
 
 client.sniped_messages = {}
 @client.event
