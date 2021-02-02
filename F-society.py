4696776f9c0b685b0ca2782dd5254b2c5a782367
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-02 14:36:09 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -38,7 +38,7 @@ async def help(ctx, arg=None):
     embed.add_field(name="• Utility",value="<a:MochaAngry:803227613669490719> Commands that'll be useful like snipes , editsnipes , 8ball",inline=True)
     embed.add_field(name="• Fun",value="<a:sleepingcat:799691148628852776> Fun Commands that is actually fun<:wot:790094440387182604>",inline=True)
     embed.add_field(name="• Reaction", value="<:EXTRASHY:788441412872962059> Non Simping Chill Reaction commands maybe ? the ones that are not cringy ofc",inline=True)
-    embed.set_footer(text="Dig Bick Energy Gang | To get more information into the command do f help_<command> ")
+    embed.set_footer(text="Dig Bick Energy Gang | To get more information into the command do f help <command> ")
     embed.add_field(name="Making in Progress",value="*So far I only have few commands! ;-;*" , inline=False)
     await ctx.send(embed=embed)
   elif arg in ["fun","FUN","Fun"]:
@@ -240,40 +240,7 @@ async def howsimp(ctx, member: discord.Member=None):
    
     
     await ctx.send(embed=em) 
-@client.command()
-async def help_fun(ctx):
-  em=discord.Embed(title="Fun Commands",color=0x2f3136)
-  em.add_field(name="• Simprate",value="**f howsimp** <a:simp:775732672793411605> Gives your simp rate!")
-  em.add_field(name="• Gayrate",value="**f howgay** <a:BIGGAY:805831470634762291> Gives your simp rate!")
-  em.add_field(name="• Hornyrate",value="**f howgay** <:hmmm:790829841679253525>  Gives your simp rate!")
-  em.set_footer(text=f'Requested by {ctx.author.name}',icon_url=ctx.author.avatar_url)
 
-  await ctx.send(embed=em)
-@client.command()
-async def help_info(ctx):
-  em=discord.Embed(title="Info Commands",color=0x2f3136)
-  em.add_field(name="• Avatar",value="<a:Chat:804180442626261014> **f avatar** Gets the avatar of the person!",inline=True)
-  em.add_field(name="• Ping",value=":placard: **f ping** Poke me... I poke you back with the BOT's Ping!",inline=True)
-  em.add_field(name="Coming Soon",value="In Progress")
-  em.set_footer(text=f'Requested by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-
-  await ctx.send(embed=em)
-@client.command()
-async def help_utility(ctx):
-  embed=discord.Embed(title="Utility Commands",color=0x2f3136)
-  embed.add_field(name="• BAM",value="<a:crown:793089465659949076> **f bam** Get Bammed! :P *Owner Abuse Only* <:Shiba_Cool:793772486822068224> ",inline=True)
-  embed.add_field(name="• 8Ball",value=":8ball: **f eiball** Ask me a simple question predicting future I'll give my opinion!",inline=True)
-  embed.add_field(name="• Snipe",value="<:Sniper:803875843507748874> **f snipe** I'll snipe the deleted message cos why not! ;)",inline=True)
-  embed.add_field(name="• Edit Snipe",value="<:Target:803960603541635072> **f editsnipe** I'll snipe the edit message cos idk ask ;)",inline=True)
-  embed.set_footer(text=f'Requested by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-  await ctx.send(embed=embed)
-@client.command()
-async def help_reaction(ctx):
-  embed=discord.Embed(title="Reaction Commands",color=0x2f3136)
-  embed.add_field(name="• Hug", value="<:EXTRASHY:788441412872962059> **f hug** hugs platonically :D",inline=True)
-  embed.add_field(name="• Kiss", value="<:oh:774246120846917633> **f kiss** kisses platonically :3",inline=True)
-  embed.add_field(name="Coming Soon",value="In Progress")
-  await ctx.send(embed=embed)
 
   
   
