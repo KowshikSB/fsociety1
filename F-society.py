dca28a6d806701ef0951970a9c95e087afb38b47
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-14 09:41:19 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -21,7 +21,7 @@ command_prefix =commands.when_mentioned_or('f ')
 
 async def status():
   while True:
-    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"on {len(client.guilds)} Servers"))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} Servers"))
     await asyncio.sleep(1800)
     await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing , name="With your mama"))
     await asyncio.sleep(1800)
