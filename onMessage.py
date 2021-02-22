9db2ad2823316fceb47880705e0e9f38c0668422
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-22 09:17:42 +0530

    Update onMessage.py

--- onMessage.py
+++ onMessage.py
@@ -4,6 +4,7 @@ from discord import client, utils
 import discord
 import asyncio
 from discord.ext import tasks
+from discord.ext.commands.core import check
 from discord.utils import get 
 
 from discord.ext.commands.converter import clean_content
@@ -34,39 +35,40 @@ Use **f close** to Close the Ticket'''
 		async def on_message(self, message):
 			if message.author.bot:
 				return
-			if message.content!='f confess' or 'F confess' and message.content=='f support' or 'F support':
-				await message.send(embed=em)
+			
 				
-				if isinstance(message.channel, discord.DMChannel):
-					guild = self.client.get_guild(725302478823751702)
-					categ = utils.get(guild.categories, name = "Modmail tickets")
-					if not categ:
-						overwrites = {
-							guild.default_role : discord.PermissionOverwrite(read_messages = False),
-							guild.me : discord.PermissionOverwrite(read_messages = True)
-						}
-						categ = await guild.create_category(name = "Modmail tickets", overwrites = overwrites)
+			await client.wait_for('f support',timeout=60,check=check)
+			if isinstance(message.channel, discord.DMChannel):
+				await message.send(embed=em)
+				guild = self.client.get_guild(725302478823751702)
+				categ = utils.get(guild.categories, name = "Modmail tickets")
+				if not categ:
+					overwrites = {
+						guild.default_role : discord.PermissionOverwrite(read_messages = False),
+						guild.me : discord.PermissionOverwrite(read_messages = True)
+					}
+					categ = await guild.create_category(name = "Modmail tickets", overwrites = overwrites)
 
-					channel = utils.get(categ.channels, topic = str(message.author.id))
-					if not channel:
-						channel = await categ.create_text_channel(name = f"{message.author.name}#{message.author.discriminator}", topic = str(message.author.id))
-						await channel.send(f"New modmail created by {message.author.mention}")
+				channel = utils.get(categ.channels, topic = str(message.author.id))
+				if not channel:
+					channel = await categ.create_text_channel(name = f"{message.author.name}#{message.author.discriminator}", topic = str(message.author.id))
+					await channel.send(f"New modmail created by {message.author.mention}")
 
-					embed = discord.Embed(description = message.content, colour = 0x2f3136)
-					embed.set_author(name = message.author, icon_url = message.author.avatar_url)
-					await channel.send(embed = embed)
+				embed = discord.Embed(description = message.content, colour = 0x2f3136)
+				embed.set_author(name = message.author, icon_url = message.author.avatar_url)
+				await channel.send(embed = embed)
 
-				elif isinstance(message.channel, discord.TextChannel):
-					if message.content.startswith(self.client.command_prefix):
-						pass
-					else:
-						topic = message.channel.topic
-						if topic:
-							member = message.guild.get_member(int(topic))
-							if member:
-								embed = discord.Embed(description = message.content, colour = 0x2f3136)
-								embed.set_author(name = message.author, icon_url = message.author.avatar_url)
-								await member.send(embed = embed)
+			elif isinstance(message.channel, discord.TextChannel):
+				if message.content.startswith(self.client.command_prefix):
+					pass
+				else:
+					topic = message.channel.topic
+					if topic:
+						member = message.guild.get_member(int(topic))
+						if member:
+							embed = discord.Embed(description = message.content, colour = 0x2f3136)
+							embed.set_author(name = message.author, icon_url = message.author.avatar_url)
+							await member.send(embed = embed)
 
 		@commands.command()
 		async def close(self, ctx):
