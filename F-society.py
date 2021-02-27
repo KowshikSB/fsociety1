9212f32f99a0764f1656011b6baf70010dc52262
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-27 22:48:37 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -574,6 +574,12 @@ async def userinfo(ctx,member:discord.Member=None):
   badges = [public_flags for public_flags in member.public_flags]
   d={'staff':'<:staff:815232627694632980>','partner':'<:partner:815232427777720340>','bug_hunter':'<:bughunter:815188215878123530>','hypesquad_bravery':'<:HypeaSquadBravery:815187983652225064>','hypesquad_brilliance':'<:HypeSquadBrilliance:815187944675475466>','hypeaquad_balance':'<:HypeSquadBalance:815187919823175721> ','early_supporter':'<:BadgeEarlySupporter:815188034655092757>','bug_hunter_level_2':'<:GoldBugHunter:815188244744372234>','verified_bot_developer':'<:EarlyVerifiedBotDeveloper:815188301707345921>','nitro':'<:nitro:815188982819848225>','boost_badges':"<a:BoostBadges:815188839487897621>"}
   s=''
+  embed=discord.Embed(color=0x2f3136,timestamp=ctx.message.created_at)
+  embed.set_author(name=f'User Info - {member}')
+  embed.set_thumbnail(url=member.avatar_url)
+  embed.set_footer(text=f'Requested by{ctx.author}',icon_url=ctx.author.avatar_url)
+  embed.add_field(name='• ID',value=f'`{member.id}`<@{member.id}>')
+  embed.add_field(name='• Nickname',value=member.display_name)
   for a in badges:
     if a[1] is True:
       
@@ -583,14 +589,10 @@ async def userinfo(ctx,member:discord.Member=None):
     s+=d['nitro']
     s+=d['boost_badges']    
 
-  embed=discord.Embed(color=0x2f3136,timestamp=ctx.message.created_at)
-  embed.set_author(name=f'User Info - {member}')
-  embed.set_thumbnail(url=member.avatar_url)
-  embed.set_footer(text=f'Requested by{ctx.author}',icon_url=ctx.author.avatar_url)
-  embed.add_field(name='• ID',value=f'`{member.id}`<@{member.id}>')
-  embed.add_field(name='• Nickname',value=member.display_name)
-  embed.add_field(name="• Joined at:",value=f'`{member.joined_at.strftime("%a,%#d %B %Y,%I:%M %p UTC`")}`',inline=False)
-  embed.add_field(name="• C`reated at:",value=f'`{member.created_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
+  embed.add_field(name='• Swaggy Server Booster',value='<@&778915337541386241> <a:nexin_pop:815257177982894091>')
+  
+  embed.add_field(name="• Joined at:",value=f'`{member.joined_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
+  embed.add_field(name="• Created at:",value=f'`{member.created_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
   
   embed.add_field(name='• Badges: ',value=s)
     
