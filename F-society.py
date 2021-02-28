1a2d0bf17dc9e4117eba5088e01473b8d7861448
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-28 07:38:48 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -596,6 +596,7 @@ async def userinfo(ctx,member:discord.Member=None):
   
   embed.add_field(name="• Joined at:",value=f'`{member.joined_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
   embed.add_field(name="• Created at:",value=f'`{member.created_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
+  print(badges)
   if badges!=[]:
     embed.add_field(name='• Badges: ',value=s)
   #testingd
