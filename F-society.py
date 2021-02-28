30d62610d2993fe333c3d5108610df215c6c51e3
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-02-28 14:45:52 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -577,6 +577,7 @@ async def userinfo(ctx,member:discord.Member=None):
   d={'staff':'<:staff:815232627694632980>','partner':'<:partner:815232427777720340>','bug_hunter':'<:bughunter:815188215878123530>','hypesquad_bravery':'<:HypeaSquadBravery:815187983652225064>','hypesquad_brilliance':'<:HypeSquadBrilliance:815187944675475466>','hypeaquad_balance':'<:HypeSquadBalance:815187919823175721> ','early_supporter':'<:BadgeEarlySupporter:815188034655092757>','bug_hunter_level_2':'<:GoldBugHunter:815188244744372234>','verified_bot_developer':'<:EarlyVerifiedBotDeveloper:815188301707345921>','nitro':'<:nitro:815188982819848225>','boost_badges':"<a:BoostBadges:815188839487897621>"}
   s=''
   
+    
   
   embed=discord.Embed(color=0x2f3136,timestamp=ctx.message.created_at)
   embed.set_author(name=f'User Info - {member}')
@@ -584,7 +585,6 @@ async def userinfo(ctx,member:discord.Member=None):
   embed.set_footer(text=f'Requested by{ctx.author}',icon_url=ctx.author.avatar_url)
   embed.add_field(name='• ID',value=f'`{member.id}` <@{member.id}>')
   embed.add_field(name='• Nickname',value=member.display_name,inline=False)
-  
   
 
   
@@ -605,7 +605,9 @@ async def userinfo(ctx,member:discord.Member=None):
   
   embed.add_field(name="• Joined at:",value=f'`{member.joined_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
   embed.add_field(name="• Created at:",value=f'`{member.created_at.strftime("%a,%#d %B %Y,%I:%M %p UTC")}`',inline=False)
-  
+  x='<a:u_Chat:790568691888029726>'
+  if roles in [788392538888601601,774493042475663370,777459196919545856,807126197652946956]:
+    embed.add_field(name=f'• {x} Staff Team',value='Kul People',inline=False)
   
   if s!='':
     embed.add_field(name='• Badges: ',value=s)
