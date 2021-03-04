12c71c63f3a137071d72b866e45fafd525bdc757
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-03-04 20:05:16 +0530

    Update F-society.py

--- F-society.py
+++ F-society.py
@@ -407,7 +407,7 @@ async def invite(ctx):
 @client.command()
 async def credits(ctx):
   e=discord.Embed(title="The F Society",color=0x2f3136)
-  x='''Creator - **KaZE#5043**
+  x='''Creator - **KaZE#0002**
   Developer - **Davy#0666**'''
 
   e.add_field(name="<:bot_dev:804257409388249098> BOT DEV",value=f"{x}",inline=False)
